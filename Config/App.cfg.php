7294ec44f600cd17b74a6a741d55708e61402054
g105b <greg.bowler@g105b.com>
2012-02-04 19:30:12 +0000

    Added documentation of class

--- Config/App.cfg.php
+++ Config/App.cfg.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * All config classes suffixed with _Framework hold configuration settings
+ * that apply to all applications. All protected properties can be overridden
+ * by application-specific versions of the class. App-specific versions are
+ * stored in the Config application directory, and the classes are named
+ * similarly, but drop the _Framework suffix.
+*/
 class App_Config_Framework {
 	protected $_activeModules;
 	protected $_isCached = true;
