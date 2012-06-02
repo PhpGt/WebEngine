67c247783a9b019d9bc8078404d7716bb7e34431
g105b <greg.bowler@g105b.com>
2012-06-02 09:00:40 +0100

    Much more simplified JavaScript, attaches helper functions to DOM Elements.

--- Script/Gt.js
+++ Script/Gt.js
@@ -26,7 +26,9 @@
 				// Callback function provided, execute on DomReady event.
 				return GT.ready(arguments[0], arguments[1]);
 			}
-			if(typeof arguments[0] === "string") {
+			if(typeof arguments[0] === "string"
+			|| arguments[0] instanceof NodeList
+			|| arguments[0] instanceof HTMLElement) {
 				// Return matching DomNodes from CSS selector.
 				return GT.dom(arguments[0], arguments[1]);
 			}
@@ -48,6 +50,63 @@
 				}
 				tmplDiv.parentNode.removeChild(tmplDiv);
 			}
+		},
+		helpers = {
+			"addClass": function(name) {
+				this.className += " " + name;
+				return this;
+			},
+			"removeClass": function(name) {
+				this.className.replace(name, "");
+				return this;
+			},
+			"hasClass": function(name) {
+				var match = new Regexp(name, "im");
+				return classname.match(match);
+			},
+			"remove": function() {
+				this.parentNode.removeChild(this);
+				return this;
+			},
+			"append": function(element) {
+				this.appendChild(element);
+				return element;
+			},
+			"prepend": function(element) {
+				this.insertBefore(element, this.firstChild);
+				return element;
+			},
+			"before": function(element) {
+				this.parentNode.insertBefore(element, this);
+				return element;
+			},
+			"after": function(element) {
+				this.parentNode.insertBefore(element, this.nextSibling);
+				return element;
+			}
+		},
+		nodeListWrap = function(me, funcName, args) {
+			var i;
+			for(i = 0; i < me.length; i++) {
+				me[i][funcName].apply(me[i], args);
+			}
+		},
+		addHelpers = function() {
+			Object.keys(helpers).map(function(key) {
+				Element.prototype[key] = helpers[key];
+				NodeList.prototype[key] = function() {
+					nodeListWrap(this, key, arguments);
+				}
+			});
+			/*Element.prototype.addClass = addClass;
+			Element.prototype.removeClass = removeClass;
+			Element.prototype.hasClass = hasClass;
+
+			NodeList.prototype.addClass = function(name) {
+				mapFunc(addClass, name)
+			};
+			NodeList.prototype.removeClass = removeClass;
+			NodeList.prototype.hasClass = hasClass;*/
 		};
 
 	GT.error = function(message) {
@@ -132,12 +191,37 @@
 	};
 
 	/**
-	 * Wrapper to querySelectorAll method.
+	 * Wrapper to querySelectorAll method. Pass an optional context node to
+	 * perform a query selection within that node.
+	 *
 	 * @param string selector The CSS selector to find.
 	 * @return DomNodeList An array containing the matching elements.
 	 */
 	GT.dom = function(selector) {
-		return document.querySelectorAll(selector);
+		var context = document;
+		if(arguments.length > 1) {
+			if(arguments[0] instanceof String) {
+				selector = arguments[0];
+			}
+			if(arguments[1] instanceof String) {
+				selector = arguments[1];
+			}
+			if(arguments[0] instanceof HTMLElement) {
+				context = arguments[0];
+				selector = arguments[1];
+			}
+			if(arguments[1] instanceof HTMLElement) {
+				context = arguments[1];
+			}
+			if(arguments[0] instanceof NodeList) {
+				context = arguments[0][0];
+				selector = arguments[1];
+			}
+			if(arguments[1] instanceof NodeList) {
+				context = arguments[1][0];
+			}
+		}
+		return context.querySelectorAll(selector);
 	};
 
 	GT.template = function(name) {
@@ -153,9 +237,68 @@
 	GT.tool = function(name) {
 
 	};
+
+	/**
+	 * Provides a really simple ajax library, intended for modern browsers.
+	 * Will automatically parse the response, converting into JSON object when
+	 * possible.
+	 *
+	 * @param string url The url to request, with parameters in the query string
+	 * for GET and POST.
+	 * @param function callback The function to call when response is ready.
+	 * @return XMLHttpRequest The XHR object.
+	 */
+	GT.ajax = new function(url, callback) {
+		var req = function(url, callback, method) {
+			var xhr,
+				method = method.toUpperCase();
+			if(window.XMLHttpRequest) {
+				xhr = new XMLHttpRequest();
+			}
+			else {
+				xhr = new ActiveXObject("Microsoft.XMLHTTP");
+			}
+			xhr.open(method, url, true);
+
+			if(method === "POST") {
+				httpRequest.setRequestHeader(
+					"Content-Type", "application/x-www-form-urlencoded");
+			}
+
+			xhr.onreadystatechange = function() {
+				var response;
+				if(xhr.readyState === 4) {
+					console.log(xhr);
+					if(callback) {
+						response = xhr.response;
+						// Quick and dirty JSON detection (skipping real
+						// detection).
+						if(xhr.response[0] === "{" || xhr.response[0] === "[") {
+							// Real JSON detection (slower).
+							try {
+								response = JSON.parse(xhr.response);
+							}
+							catch(e) {}
+						}
+						callback(response, xhr);
+					}
+				}
+			};
+
+			xhr.send();
+			return xhr;
+		};
+		this.get = function(url, callback) {
+			return req(url, callback, "get");
+		};
+		this.post = function(url, callback) {
+			return req(url, callback, "post");
+		};
+	};
 	window.GT = GT;
 
 	// Perform automatic template collection.
 	// The template elements are provided by PHP.Gt just before DOM flushing.
 	GT(templateScrape);
+	GT(addHelpers);
 }());
