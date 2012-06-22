0091831cc118f1f7440e83ba1cef337ab28f2221
g105b <greg.bowler@g105b.com>
2012-06-22 08:40:21 +0100

    Updated user table to allow for full name and lookup fields

--- Database/User/GetAll.sql
+++ Database/User/GetAll.sql
@@ -1,3 +1,14 @@
 select
-	*
+	*,
+	(select `Name`
+		from `User_Type`
+		where `User_Type`.`Id` = `Fk_User_Type`
+	) as `L_User_Type`,
+	`Fk_User_Department`,
+	(select `Name`
+		from `User_Department`
+		where `User_Department`.`Id` = `Fk_User_Department`
+	) as `L_User_Department`,
+	(select concat(`FirstName`, " ", `LastName`)
+	) as `FullName`
 from `User`
--- Database/User/GetByUuid.sql
+++ Database/User/GetByUuid.sql
@@ -1,5 +1,16 @@
 select
-	*
+	*,
+	(select `Name`
+		from `User_Type`
+		where `User_Type`.`Id` = `Fk_User_Type`
+	) as `L_User_Type`,
+	`Fk_User_Department`,
+	(select `Name`
+		from `User_Department`
+		where `User_Department`.`Id` = `Fk_User_Department`
+	) as `L_User_Department`,
+	(select concat(`FirstName`, " ", `LastName`)
+	) as `FullName`
 from `User`
 where `Uuid` = :Uuid
 limit 1;
