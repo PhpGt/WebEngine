61e0ccd94e9d13e90d307075136efbba9bc6ea92
Greg Bowler <greg.bowler@g105b.com>
2012-06-22 16:29:05 +0100

    Implemented automatic case checking. 301 redirects to correctly-cased URL, fixes #19

--- Framework/Error/HttpError.php
+++ Framework/Error/HttpError.php
@@ -14,19 +14,51 @@ class HttpError extends Exception {
 		503 => "Service Unavailable"
 	);
 
+
 	public function __construct(
-	$code, $description = null, Exception $previous = null) {
+	$code, $data = null, Exception $previous = null) {
+		// On 404 errors, check case recursively.
+		if($code === 404) { 
+			// If correct cased URL found, checkCase will throw 301 error,
+			// script will end here.
+			$this->checkCase();
+		}
+
 		if(array_key_exists($code, $this->_errorCodeMessage)) {
 			$message = $this->_errorCodeMessage[$code];
 		}
 		
 		$this->sendHeaders($code, $message);
-		$this->displayError($code, $description);
+		if(is_string($data)) {
+			$this->displayError($code, $data);
+		}
+		else if(is_array($data)) {
+			$this->sendHeaders($data);
+		}
 		exit;
 	}
 
-	private function sendHeaders($code, $message) {
-		header($_SERVER["SERVER_PROTOCOL"] . " " . $code . " " . $message);
+	/**
+	 * Sends raw HTTP headers using HTTP/1.1 specification or kvp specification.
+	 * @param int|array $header Either the numerical HTTP/1.1 code, or an array
+	 * containing key-value-pairs to send.
+	 * @param string $value Optional. The associated string to send with a
+	 * HTTP/1.1 code. Not required if kvp sent in first argument.
+	 */
+	private function sendHeaders() {
+		$args = func_get_args();
+		if(is_int($args[0]) && is_string($args[1])) {
+			header($_SERVER["SERVER_PROTOCOL"]
+				. " "
+				. $args[0] 
+				. " " 
+				. $args[1]);
+		}
+		else if(is_array($args[0])) {
+			foreach ($args[0] as $key => $value) {
+				header($key . ": " . $value);
+			}
+		}
 	}
 
 	private function displayError($code, $description = "") {
@@ -78,6 +110,67 @@ class HttpError extends Exception {
 				}
 			}
 		}
+	}
+
+	private function checkCase() {
+		$dirList = explode("/", DIR);
+		$path = APPROOT . DS . "PageView";
+		$originalPath = $path;
+
+		foreach($dirList as $dir) {
+			if(empty($dir)) {
+				continue;
+			}
+
+			$dh = opendir($path);
+			while(false !== ($name = readdir($dh)) ) {
+				if(strtolower($dir) == strtolower($name)) {
+					$dir = $name;
+					$originalDir = $name;
+				}
+			}
+			closedir($dh);
+
+			$path .= DS . $dir;
+			$originalPath .= DS . $originalDir;
+		}
+
+		$fileName = FILE . "." . EXT;
+
+		// Replace directory separator with forward slash for URL.
+		$url = str_replace(DS, "/", $path);
+		// Remove APPROOT/PageView/ from beginning of path.
+		$url = substr(
+			$url, 
+			stripos($url, "PageView") + strlen("PageView")
+		);
+		// Add hostname to beginning of path.
+		$url = "http"
+			. (empty($_SERVER["HTTPS"]) ? "" : "s")
+			. "://"
+			. $_SERVER["HTTP_HOST"]
+			. $url . "/";
+
+		if($path !== $originalPath) {
+			// Add fileName to the path
+			$url .= $fileName;
+
+			// Redirect to new path.
+			throw new HttpError(301, array("Location" => $url));
+		}
+
+		// At this point, the directory path is either the correct case, or
+		// there isn't a correct alternative to the one supplied.
+		$dh = opendir($path);
+		while(false !== ($name = readdir($dh)) ) {
+			if(strtolower($name) == strtolower($fileName)) {
+				// Add fileName to the path
+				$url .= $name;
+				// Redirect to new path.
+				throw new HttpError(301, array("Location" => $url));
+			}
+		}
+		closedir($path);
 	}
 }
 ?>
