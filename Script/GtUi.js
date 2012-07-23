d0c5c5ce9d2d9eca080641cbccfe561389ea0c4a
Greg Bowler <greg.bowler@g105b.com>
2012-07-23 11:20:41 +0100

    Added some functionality to GtUi including linear interpolation with optional smoothing

--- Script/GtUi.js
+++ Script/GtUi.js
@@ -1,3 +1,30 @@
+(function() {
+	GT.ui = function() {
+	};
+	/**
+	 * Provides linear interpolation between two points with optional smoothing.
+	 */
+	GT.ui.lerp = function(start, end, scalar, smoothing) {
+		var interpolant;
+		if(smoothing === true) {
+			scalar = GT.ui.smooth(scalar);
+		}
+		else if(typeof smoothing === "function") {
+			scalar = smoothing(scalar);
+		}
+		interpolant = (end - start) * scalar;
+
+		return interpolant;
+	};
+
+	/**
+	 * Converts a scalar into a smoothed scalar.
+	 */
+	GT.ui.smooth = function(scalar) {
+		return (-Math.cos(Math.PI * scalar) + 1) / 2;
+	};
+})();
+
 GT(function() {
 	var scrollHorizontal = function(e) {
 		this.scrollLeft -= e.wheelDelta / 2;
