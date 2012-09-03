8267f22d801f85c2023caf50131f2b319b8e77c5
g105b <greg.bowler@g105b.com>
2012-09-03 15:30:57 +0100

    Started implementing Cache handler for issue #32

---
+++ Framework/Component/Cache.cmp.php
@@ -0,0 +1,75 @@
+<?php class Cache implements ArrayAccess {
+/**
+ * The PHP.Gt Cache Handler allows bi-directional access to cached resources
+ * from anywhere in the code. Within PageCode or PageTool, cached resources
+ * can be created first by instantiating the cache object and using the object
+ * as an array to get and set cache data.
+ */
+
+private $_type = null;
+private $_reference = null;
+private $_cacheDir;
+
+public function __construct($type, $reference = null) {
+	$this->_type = $type;
+	$this->_reference = $reference;
+	$this->_cacheDir = APPROOT . DS . "Cache" . DS . $type;
+}
+
+/**
+ * Checks the validity of the cache.
+ */
+public function offsetExists($offset) {
+	switch($this->_type) {
+	case "Database":
+		return $this->checkValidDatabase($offset);
+		break;
+	default:
+		die("Cache: Not yet implemented.");
+		break;
+	}
+}
+
+/**
+ * Obtains data stored in the cache.
+ */
+public function offsetGet($offset) {
+}
+
+/**
+ * Caches arbitary data for use in recurring requests.
+ */
+public function offsetSet($offset, $value) {
+}
+
+/**
+ * Used to invalidate the current cache.
+ */
+public function offsetUnset($offset) {
+}
+
+/**
+ * Checks the validity of the cache for the supplied database name, if any.
+ * Called externally by using isset($cacheObj["TableName"]).
+ *
+ * @return bool True if the cache is valid.
+ */
+private function checkValidDatabase($tableName) {
+	$cacheDir = $this->_cacheDir . DS . $tableName;
+	$touchFile = $cacheDir . ".dbtouch";
+
+	// If there is no cachedir, there definitely is no cache set.
+	if(is_dir($cacheDir)) {
+		$touchT = file_exists($touchFile)
+			? filemtime($touchFile)
+			: 0;
+		$cacheT = filemtime($cacheDir);
+		if($cacheT > $touchT) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+}?>
