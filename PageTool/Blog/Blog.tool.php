4d86c8a2350ac4ac7703c8c365e3d10a22caad60
g105b <greg.bowler@g105b.com>
2012-09-10 13:07:35 +0100

    Updated blog tool

--- PageTool/Blog/Blog.tool.php
+++ PageTool/Blog/Blog.tool.php
@@ -24,13 +24,89 @@ public function go($api, $dom, $template, $tool) {
 		exit;
 	}
 
-	$dom["h1#blogTitle"]->text = $blog["Title"];
-	$dom["span#blogDate time"]->text = date("jS F Y",
-		strtotime($blog["Dt_Publish"]));
-	$dom["span#blogDate time"]->datetime = date("Y-m-d",
-		strtotime($blog["Dt_Publish"]));
+	// Attempt to find the container for the blog.
+	$container = $dom["body > section#st_article"];
+	if($container->length == 0) {
+		$container = $dom["body > section"];
+	}
+	if($container->length == 0) {
+		$container = $dom["body"];
+	}
+	$this->output($blog, $container);
+}
 
-	$dom["div#blogContent"]->html = $blog["Content"];
+public function output($blog, $domEl) {
+	$blogApi = $this->_api["Blog"];
+	$dom = $this->_dom;
+	$template = $this->_template;
+
+	$article = null;
+
+	if($domEl->hasClass("preview") || $domEl->hasClass("previewList")) {
+		if($blog["Is_Featured"]) {
+			$article = $template["ArticleFeatured"];
+		}
+	}
+	if(is_null($article)) {
+		$article = $template["Article"];
+	}
+
+	// Check to see if there is a link within the h1.
+	$anchor = $article["header h1 a"];
+	if($anchor->length > 0) {
+		$anchor->text = $blog["Title"];
+		$article["header h1 a, footer p a"]->href = 
+			$this->getUrl($blog);
+	}
+	else {
+		$article["header h1"]->text = $blog["Title"];
+	}
+
+	$article["header time"]->setAttribute("datetime", date(
+		"Y-m-d", strtotime($blog["Dt_Publish"])));
+	$article["header time"]->text = date(
+		"jS F Y", strtotime($blog["Dt_Publish"]));
+	
+	$content = $article["div.preview, div.content"];
+	$content->html = $blog["Content"];
+	//$section->append($article); // wtfisthis?
+
+	// Get all the tags for the article.
+	$ulTags = $article["header ul.tags"];
+	$ulTags->removeChildren();
+
+	$tagList = $blogApi->getTags(["Id" => $blog["Id"]]);
+	foreach ($tagList as $tag) {
+		$li = $dom->create("li", ["data-tag-id" => $tag["Id"]]);
+		$a = $dom->create("a", [
+			"href" => $this->getTagUrl($tag)],
+			$tag["Name"]);
+		$li->append($a);
+		$ulTags->append($li);
+	}
+
+	$domEl->append($article);
+
+	if($article["div.preview"]->length <= 0) {
+		return;
+	}
+
+	$foundPreview = false;
+	$node = $article["div.preview"]->firstChild;
+
+	while(!is_null($node)) {
+		if($foundPreview) {
+			$removeNode = $node;
+		}
+		if($node->hasClass("previewBreak")) {
+			$foundPreview = true;
+		}
+
+		$node = $node->nextSibling;
+		if(!empty($removeNode)) {
+			$removeNode->remove();
+		}
+	}
 }
 
 public function setName($name) {
