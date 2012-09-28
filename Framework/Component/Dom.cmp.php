b346cc4997e45cdf19a2c1385da3d1dff8aa58f8
Greg Bowler <greg.bowler@g105b.com>
2012-09-28 08:26:15 +0100

    Keeps data-template attribute after initial templation

--- Framework/Component/Dom.cmp.php
+++ Framework/Component/Dom.cmp.php
@@ -218,7 +218,6 @@ public function template($attribute = "data-template") {
 	for($i = 0; $i < $domNodeListLength; $i++) {
 		$item = $domNodeList->item($i);
 		$attr = $item->getAttribute($attribute);
-		$item->removeAttribute($attribute);
 		if(isset($domNodeArray[$attr])) {
 			$item->parentNode->removeChild($item);
 		}
