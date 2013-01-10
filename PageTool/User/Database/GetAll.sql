a0dca44c98ea199c1452d653dd3234d9401196cf
g105b <greg.bowler@g105b.com>
2013-01-10 17:34:30 +0000

    Removed supplementary data from User tables

--- PageTool/User/Database/GetAll.sql
+++ PageTool/User/Database/GetAll.sql
@@ -1,9 +1,7 @@
 select
 	*,
 	`User`.`Id`,
-	`User_Type`.`Name` as `User_Type_Name`,
-	(select concat(`FirstName`, " ", `LastName`)
-	) as `FullName`
+	`User_Type`.`Name` as `User_Type_Name`
 from `User`
 inner join `User_Type`
 	on (`User_Type`.`Id` = `User`.`Fk_User_Type`);
