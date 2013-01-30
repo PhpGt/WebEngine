b42c3a3cb65afec9206265a6319e18a63044bf15
g105b <greg.bowler@g105b.com>
2013-01-30 12:30:50 +0000

    Removed * selector in queries, removed backticks.

--- PageTool/User/Database/GetByID.sql
+++ PageTool/User/Database/GetByID.sql
@@ -1,9 +1,12 @@
 select
-	*,
-	`User`.`ID`,
-	`User_Type`.`name` as `User_Type__name`
-from `User`
-inner join `User_Type`
-	on (`User_Type`.`ID` = `User`.`FK_User_Type`)
-where `User`.`ID` = :ID
+	User.ID,
+	User.username,
+	User.dateTimeIdentified,
+	User.dateTimeLastActive,
+	User_Type.name as User_Type__name
+from User
+inner join User_Type
+	on (User_Type.ID = User.FK_User_Type)
+where User.ID = :ID
+and User.dateTimeDeleted is null
 limit 1;
