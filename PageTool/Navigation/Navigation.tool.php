53dba274feeb8ec8c54b0c0d9a6d81235bb51718
g105b <greg.bowler@g105b.com>
2013-02-05 16:00:57 +0000

    Tidied up code, removed comments.

--- PageTool/Navigation/Navigation.tool.php
+++ PageTool/Navigation/Navigation.tool.php
@@ -1,7 +1,7 @@
 <?php class Navigation_PageTool extends PageTool {
 /**
- * TODO: Docs.
- * Simply adds a class of "selected" to the relevant "body nav li".
+ * Used to automate an application or website's navigation system.
+ * Adds a class of "selected" to the relevant "body nav li".
  */
 private $_navElements;
 
@@ -16,29 +16,11 @@ public function go($api, $dom, $template, $tool) {
 		$navLiTags = $nav["ul li, ol li"];
 
 		foreach($navLiTags as $li) {
-			// $dir = DIR;
-			// if(strstr($dir, "/")) {
-			// 	$dir = substr($dir, 0, strpos($dir, "/"));
-			// }
-
-			// $toMatch = strlen($dir) > 0
-			// 	? $dir
-			// 	: FILE;
-			// $pattern = "/" . $toMatch . "/";
-
-			// if (strstr(DIR, "/")) {
-			// 	// var_dump($pattern, FILE);die();
-			// }
-
-			// if(FILE === "Index") {
-			// 	// Match "Index" with "/".
-			// 	$pattern = "/^" . DIR . "\/$|Index/";
-			// }
-
 			if($li->hasAttribute("data-selected-pattern")) {
 				$pattern = $li->getAttribute("data-selected-pattern");
 
-			} else {
+			}
+			else {
 				$pattern = "/$target/";
 			}
 			
