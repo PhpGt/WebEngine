17bdb5dc13f1183d6fb6952a0c3a2ac811d8f0d5
Greg Bowler <greg.bowler@g105b.com>
2013-05-28 08:16:59 +0100

    Added user profile dependency for Hybrid Auth

--- Class/ClassDependencies.php
+++ Class/ClassDependencies.php
@@ -8,6 +8,7 @@
 public static $list = array(
 	"Hybrid_Auth" => "Auth/hybridauth/Hybrid/Auth.php",
 	"Hybrid_Endpoint" => "Auth/hybridauth/Hybrid/Endpoint.php",
+	"Hybrid_User_Profile" => "Auth/hybridauth/Hybrid/User_Profile.php",
 );
 
 }#
