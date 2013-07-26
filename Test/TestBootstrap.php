723cd30e36e43024271da793626828512b596a33
Greg Bowler <greg.bowler@g105b.com>
2013-07-26 15:18:24 +0100

    Added recursive copy function to bootstrap to copy TestApp_Base

--- Test/TestBootstrap.php
+++ Test/TestBootstrap.php
@@ -1,2 +1,19 @@
 <?php // TestBootstrap.php - loads environment variables required by all tests.
 define("GTROOT", getcwd() . "/../");
+
+function createTestApp() {
+	$source = "TestApp_Base";
+	$dest = "TestApp";
+
+	foreach ($iterator = new RecursiveIteratorIterator(
+		new RecursiveDirectoryIterator(
+			$source, RecursiveDirectoryIterator::SKIP_DOTS),
+	RecursiveIteratorIterator::SELF_FIRST) as $item	) {
+
+		if ($item->isDir()) {
+			mkdir("$dest/" . $iterator->getSubPathName());
+		} else {
+			copy($item, "$dest/" . $iterator->getSubPathName());
+		}
+	}
+}
