3bec2baae2a3b457e7136c20c9f2742521baa15c
Greg Bowler <greg.bowler@g105b.com>
2013-11-11 16:42:45 +0000

    Add table names to all fields.

--- PageTool/Blog/Database/GetArticles.sql
+++ PageTool/Blog/Database/GetArticles.sql
@@ -5,14 +5,14 @@ select
 	Blog_User.username,
 	Blog_User.firstName,
 	Blog_User.lastName,
-	title,
-	content,
-	coverImageUrl,
-	dateTimeCreated,
-	dateTimePublished,
-	dateTimeFeatured,
-	dateTimeUnfeatured,
-	count(Blog_Article_Comment) as commentCount
+	Blog_Article.title,
+	Blog_Article.content,
+	Blog_Article.coverImageUrl,
+	Blog_Article.dateTimeCreated,
+	Blog_Article.dateTimePublished,
+	Blog_Article.dateTimeFeatured,
+	Blog_Article.dateTimeUnfeatured,
+	count(Blog_Article_Comment.ID) as commentCount
 from
 	Blog_Article
 
@@ -24,9 +24,18 @@ inner join Blog_User
 on
 	Blog_Article.FK_Blog_User__author = Blog_User.ID
 
+-- Left join because there may not necessarily be any comments.
+left join Blog_Article_Comment
+on
+	Blog_Article.ID = Blog_Article_Comment.FK_Blog_Article
+
 where
 	Blog.name = :name_Blog
 and
 	dateTimeDeleted is null
+
+-- We group on the comment's ID field so that the count() function doesn't
+-- force an empty row on an empty result. 
+group by Blog_Article_Comment.ID
 
 limit :Limit
