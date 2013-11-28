b31764104b82b332492354c0633f70f5e8d403ea
Greg Bowler <greg.bowler@g105b.com>
2013-11-28 11:38:23 +0000

    Strip back FileOrganiser and ClientSideCompiler files.

--- Framework/ClientSideCompiler.php
+++ Framework/ClientSideCompiler.php
@@ -1,13 +1,7 @@
 <?php final class ClientSideCompiler {
 /**
- * The ClientSideCompiler pre-processes necessary files, converting them into
- * their processed equivalents, removing the originals. If App_Config has the
- * client-side compilation enabled, all resources will be minified and compiled
- * into a single resource.
- *
- * The DOM is updated to only include the necessary files, so the HTML should
- * reference all JS and CSS/SCSS files without the worry of multiple requests
- * or unminified scripts being exposed publicly.
+ * The ClientSideCompiler minifies necessary files, removes the originals,
+ * according to App_Config's client-side compilation and production flags.
  */
 private $_compileFunctionList = array(
 	"js" => "javaScript",
