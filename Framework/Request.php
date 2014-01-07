bfc8eca543d6f6e0a144f2736f8ef831b0c08cf0
Greg Bowler <greg.bowler@g105b.com>
2014-01-07 14:40:47 +0000

    Update startTime to use Session class

--- Framework/Request.php
+++ Framework/Request.php
@@ -14,10 +14,11 @@ public $contentType;
 public $pageCodeStop = false;
 
 public function __construct($config, $t) {
-	$_SESSION["PhpGt_StartTime"] = $t;
-	date_default_timezone_set($config["App"]::getTimezone());
 	$this->contentType = "text/html";
 	session_start();
+	$_SESSION["PhpGt_StartTime"] = $t;
+	Session::set("Gt.startTime", $t);
+	date_default_timezone_set($config["App"]::getTimezone());
 
 	// Allow language selection through special URI syntax.
 	$bd = BASEDIR;
