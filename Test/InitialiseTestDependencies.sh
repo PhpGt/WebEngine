b059e276de0230c94e2f98bfd353bd3f30677855
Greg Bowler <greg.bowler@g105b.com>
2014-02-06 14:45:09 +0000

    Add chrome driver and selenium

--- Test/InitialiseTestDependencies.sh
+++ Test/InitialiseTestDependencies.sh
@@ -20,5 +20,19 @@ else
     php composer.phar self-update
 fi
 
+if [[ ! -e "chromedriver" ]]
+then
+	wget -N http://chromedriver.storage.googleapis.com/2.9/chromedriver_linux64.zip
+	unzip chromedriver_linux64.zip
+	rm chromedriver_linux64.zip
+fi
+chmod +x chromedriver
+
+if [[ ! -e "selenium-server-standalone-2.39.0.jar" ]]
+then
+	wget -N http://selenium.googlecode.com/files/selenium-server-standalone-2.39.0.jar
+fi
+chmod +x selenium-server-standalone-2.39.0.jar
+
 php composer.phar install --dev
 exit 0
