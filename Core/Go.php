2fc17c87c71321b030b3d236bd27b026a791fe22
Greg Bowler <greg.bowler@g105b.com>
2014-06-27 17:21:25 +0100

    Add Go.php router script

---
+++ Core/Go.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Router script used by gtserver.php.
+ *
+ * PHP.Gt (http://php.gt)
+ * @copyright Copyright Ⓒ 2014 Bright Flair Ltd. (http://brightflair.com)
+ * @license Apache Version 2.0, January 2004. http://www.apache.org/licenses
+ */
