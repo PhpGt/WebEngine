4cf972eab4de80f6295dedc2505429e44b88b0c0
Greg Bowler <greg.bowler@g105b.com>
2014-06-27 18:24:03 +0100

    Implement static web server
    For #179

--- Core/Router.php
+++ Core/Router.php
@@ -2,6 +2,9 @@
 /**
  * Router script used by gtserver.php.
  *
+ * Will serve all directory-style URLs through PHP.Gt. Files (URLs with 
+ * file extensions) will have their bytes streamed and correct HTTP headers set.
+ *
  * PHP.Gt (http://php.gt)
  * @copyright Copyright Ⓒ 2014 Bright Flair Ltd. (http://brightflair.com)
  * @license Apache Version 2.0, January 2004. http://www.apache.org/licenses
@@ -11,3 +14,27 @@ if(php_sapi_name() !== "cli-server") {
 	exit(1);
 }
 
+$pathinfo = pathinfo($_SERVER["REQUEST_URI"]);
+if(!empty($pathinfo["extension"])) {
+	// Non-empty extension is served as static file.
+	$request = explode("?", 
+		$_SERVER["DOCUMENT_ROOT"]
+		.
+		$_SERVER["REQUEST_URI"]
+	);
+
+	if(!is_file($request[0])) {
+		return false;
+	}
+
+	$ext = pathinfo($request[0], PATHINFO_EXTENSION);
+	$finfo = new Finfo(FILEINFO_MIME_TYPE);
+	$mime = $finfo->file($request[0]);
+
+	header("Content-type: $mime");
+	readfile($request[0]);
+	return true;
+}
+
+// Request is to a PHP.Gt PageView or WebService.
+// TODO: Pass to the PHP.Gt bootstrapper.
