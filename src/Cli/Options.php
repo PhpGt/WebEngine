6a9b68cf687602840b94a300bec982b82cf29c5c
Greg Bowler <greg.bowler@g105b.com>
2014-06-30 17:04:01 +0100

    Introduce bin, src, test direcories.
    For #180, #182

--- src/Cli/Options.php
+++ src/Cli/Options.php
@@ -1,14 +1,10 @@
-#!/usr/bin/env php
 <?php
 /**
- * Shell script interface for launching the built-in webserver.
- *
  * PHP.Gt (http://php.gt)
  * @copyright Copyright Ⓒ 2014 Bright Flair Ltd. (http://brightflair.com)
  * @license Apache Version 2.0, January 2004. http://www.apache.org/licenses
  */
 namespace Gt\Cli;
-require(__DIR__ . "/../Package/autoload.php");
+class Options {
 
-$options = new Options();
-return new Server($options->getArgs());
+}#
---
+++ src/Cli/Server.php
@@ -0,0 +1,192 @@
+<?php
+/**
+ * Used as a wrapper to the PHP built-in server to handle directory paths and
+ * alert the developer if directories do not exist, before starting the server.
+ *
+ * This script should be executed from the base directory of the PHP.Gt
+ * application wishing to be served (the "approot"), either by referencing the
+ * script absolutely, or by having it within the user's environment path.
+ * Alternatively, the base directory of the PHP.Gt application (the "gtroot")
+ * can be passed as the --approot argument.
+ *
+ * This script is intended to be run from CLI, so a self-instantiating class is
+ * required. When not run from CLI, the constructor does not call inner methods.
+ * This allows for proper unit testing.
+ *
+ * PHP.Gt (http://php.gt)
+ * @copyright Copyright Ⓒ 2014 Bright Flair Ltd. (http://brightflair.com)
+ * @license Apache Version 2.0, January 2004. http://www.apache.org/licenses
+ */
+namespace Gt\Cli;
+final class Server {
+
+private $gtroot;
+private $approot;
+private $port = 8080;
+
+// Used by getopt for parsing cli arguments.
+// See php.net/manual/en/function.getopt.php
+private $opt_char = [
+	"no-value" => "",
+	"required" => ":",
+	"optional" => "::",
+];
+
+// Define each option and associated short-option character(s):
+private $opts = [
+	"no-value" => [
+		"help" => [
+			"shortopt" => "h",
+			"description" => "Displays this help message",
+		],
+	],
+	"required" => [
+		// No required arguments.
+	],
+	"optional" => [
+		"approot" => [
+			"shortopt" => "a",
+			"description" => "Application root directory",
+			"value" => null,
+		],
+		"port" => [
+			"shortopt" => "p",
+			"description" => "Port to bind webserver on",
+			"value" => 8080,
+		],
+	],
+];
+
+public function __construct() {
+	$this->gtroot = dirname(__DIR__);
+	$this->approot = getcwd();
+
+	// Only automatically run when being executed from cli, to allow testing.
+	if(php_sapi_name() === "cli") {
+		$this->getOptions();
+		$this->run();
+	}
+}
+
+/**
+ * Handles user input after parsing cli arguments.
+ * A key-value-pair is passed as the two parameters to the method. 
+ * 
+ * @param string $name The argument name, passed via cli.
+ * @param string $value The argument value, passed via cli.
+ */
+public function call($name, $value) {
+	switch($name) {
+
+	case "help":	// Output a list of all possible commands.
+		echo "\nUsage: gtserver [-a|--approot=$APPROOT] [-p|--port=$PORT]\n\n";
+		
+		foreach($this->opts as $opt_type => $opt_array) {
+			foreach($opt_array as $opt_name => $opt_detail) {
+				$str = "\t--$opt_name";
+				foreach (str_split($opt_detail["shortopt"]) as $opt_char) {
+					$str .= ", -$opt_char";
+				}
+
+				$str = str_pad($str, 24, " ");
+				$str .= $opt_detail["description"];
+				echo $str . "\n";
+			}
+		}
+		echo "\nFor support, see "
+			. "https://github.com/BrightFlair/PHP.Gt/wiki/built-in-server";
+		echo "\n\n";
+		exit;
+		break;
+
+	case "approot":		// Set the application directory root.
+		if(!is_dir($value)) {
+			echo "ERROR: Provided approot is not a directory.\n";
+			exit(1);
+		}
+		$this->approot = $value;
+		break;
+
+	case "port":		// Set the port used by the server.
+		if(!ctype_digit($value)
+		|| $value < 1024
+		|| $value > 65535) {
+			echo "ERROR: Provided port is not a valid.\n";
+			exit(1);
+		}
+		$this->port = $value;
+		break;
+
+	default:
+		break;
+	}
+}
+
+public function getOptions() {
+	// Characters used to build command line options.
+	// See php.net/manual/en/function.getopt.php
+
+	// Build opts_long and opts_short for passing to getopt function below.
+	$opts_long = [];
+	$opts_short = "";
+	foreach($this->opts as $opt_type => $opt_array) {
+		foreach ($opt_array as $opt_name => $opt_detail) {
+			$opts_long[] = $opt_name . $this->opt_char[$opt_type];
+			$opts_short .= implode(
+				$this->opt_char[$opt_type],
+				str_split($opt_detail["shortopt"])
+			) . $this->opt_char[$opt_type];
+		}
+	}
+	$options = getopt($opts_short, $opts_long);
+
+	// Act on passed arguments:
+	foreach($this->opts as $opt_type => $opt_array) {
+		foreach ($opt_array as $opt_name => $opt_detail) {
+			$argValue = null;
+
+			if(isset($options[$opt_name])) {
+				$argValue = $options[$opt_name];
+			}
+			else {
+				foreach (str_split($opt_detail["shortopt"]) as $opt_char) {
+					if(isset($options[$opt_char])) {
+						$argValue = $options[$opt_char];
+					}
+				}
+			}
+
+			if(!is_null($argValue)) {
+				$this->call($opt_name, $argValue);
+			}
+		}
+	}
+}
+
+/**
+ * Runs the PHP process with built-in server mode enabled.
+ */
+public function run() {
+	$fp = popen(
+		"php -S localhost:{$this->port} "
+		. "-t {$this->approot}/www "
+		. "{$this->gtroot}/Core/Router.php "
+		// Redirect STDOUT to this process's STDIN.
+		. "1>&0", 
+		"r"
+	);
+
+	if(!is_resource($fp)) {
+		echo "ERROR: Could not open php built-in server.\n";
+		exit(1);
+	}
+
+	// Pass back all output from newly-spawned process.
+	while(false !== ($s = fread($fp, 1024)) ) {
+		echo $s;
+	}
+
+	pclose($fp);	
+}
+
+}#
