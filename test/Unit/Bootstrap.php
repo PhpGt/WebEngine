972e10fd468e6bd0ebb79691db5311d3d1b1dd3c
Greg Bowler <greg.bowler@g105b.com>
2014-07-01 22:31:40 +0100

    Fix whitespace

--- test/Unit/Bootstrap.php
+++ test/Unit/Bootstrap.php
@@ -1,5 +1,11 @@
 <?php
 /**
+ * Test bootstrapper, used by PHPUnit.
+ * 
+ * At the moment this file only has one task to require the Composer autoloader,
+ * but is present to allow unit test complexity to grow in the future. May be
+ * removed in the future if not required.
+ * 
  * PHP.Gt (http://php.gt)
  * @copyright Copyright Ⓒ 2014 Bright Flair Ltd. (http://brightflair.com)
  * @license Apache Version 2.0, January 2004. http://www.apache.org/licenses
