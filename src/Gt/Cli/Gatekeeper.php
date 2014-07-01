d50f0e24085afb158d248106fd04ab14c5967223
Greg Bowler <greg.bowler@g105b.com>
2014-07-01 22:28:53 +0100

    Add phpdocs

--- src/Gt/Cli/Gatekeeper.php
+++ src/Gt/Cli/Gatekeeper.php
@@ -4,6 +4,9 @@
  * Procedural code below is necessary when triggered from inbuilt webserver
  * as this script is the point-of-entry for the whole application.
  * 
+ * Uses Gateway class to decide whether to serve static content or begin 
+ * request / response within PHP.Gt.
+ * 
  * PHP.Gt (http://php.gt)
  * @copyright Copyright Ⓒ 2014 Bright Flair Ltd. (http://brightflair.com)
  * @license Apache Version 2.0, January 2004. http://www.apache.org/licenses
@@ -11,6 +14,7 @@
 namespace Gt\Cli;
 require_once(__DIR__ . "/../../../vendor/autoload.php");
 
+// Only allow this script to be invoked from inbuilt webserver.
 if(php_sapi_name() === "cli-server") {
 	if(Gateway::isStaticFileRequest($_SERVER["REQUEST_URI"])) {
 		$filePath = Gateway::getAbsoluteFilePath($_SERVER["REQUEST_URI"]);
