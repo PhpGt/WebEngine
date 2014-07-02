1b21398ff5198d37a0d79513d377cdb1e83dd3ff
Greg Bowler <greg.bowler@g105b.com>
2014-07-02 17:40:05 +0100

    Use require for composer autoloader

--- src/Gt/Cli/Gatekeeper.php
+++ src/Gt/Cli/Gatekeeper.php
@@ -12,7 +12,7 @@
  * @license Apache Version 2.0, January 2004. http://www.apache.org/licenses
  */
 namespace Gt\Cli;
-require_once(__DIR__ . "/../../../vendor/autoload.php");
+require(__DIR__ . "/../../../vendor/autoload.php");
 
 // Only allow this script to be invoked from inbuilt webserver.
 if(php_sapi_name() === "cli-server") {
