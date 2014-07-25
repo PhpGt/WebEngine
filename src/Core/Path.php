38b851d0b0c8a5ea0f7d414f257c867293851f7e
Greg Bowler <greg.bowler@g105b.com>
2014-07-25 15:34:56 +0100

    Remove unreachable breaks

--- src/Core/Path.php
+++ src/Core/Path.php
@@ -31,70 +31,54 @@ public static function get($name) {
 	switch($name) {
 	case self::DATABASE:
 		return self::get(self::SRC) . "/Database";
-		break;
 
 	case self::PAGE:
 		return self::get(self::SRC) . "/Page";
-		break;
 
 	case self::PAGECODE:
 		return self::get(self::PAGE) . "/Code";
-		break;
 
 	case self::PAGETOOL:
 		return self::get(self::PAGE) . "/Tool";
-		break;
 
 	case self::PAGEVIEW:
 		return self::get(self::PAGE) . "/View";
-		break;
 
 	case self::PUBLICFILES:
 		return self::get(self::SRC) . "/PublicFiles";
-		break;
 
 	case self::ROOT:
 		return dirname($_SERVER["DOCUMENT_ROOT"]);
-		break;
 
 	case self::SCRIPT:
 		return self::get(self::SRC) . "/Script";
-		break;
 
 	case self::API:
 		return self::get(self::SRC) . "/API";
-		break;
 
 	case self::APICODE:
 		return self::get(self::API) . "/Code";
-		break;
 
 	case self::APITOOL:
 		return self::get(self::API) . "/Tool";
-		break;
 
 	case self::APIVIEW:
 		return self::get(self::API) . "/View";
-		break;
 
 	case self::SRC:
 		return self::get(self::ROOT) . "/src";
-		break;
 
 	case self::STYLE:
 		return self::get(self::SRC) . "/Style";
-		break;
 
 	case self::WWW:
 		return self::get(self::ROOT) . "/www";
-		break;
 
 	case self::GTROOT:
 		return realpath(__DIR__ . "/../../");
 
 	default:
 		throw new \UnexpectedValueException("Invalid path: $name");
-		break;
 	}
 }
 
