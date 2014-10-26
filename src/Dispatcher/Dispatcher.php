c2e2aa2b09fdd77d1f63c1dac9b299a28715b96f
Greg Bowler <greg.bowler@g105b.com>
2014-10-26 22:18:09 +0000

    Implement templatefactory getter

--- src/Dispatcher/Dispatcher.php
+++ src/Dispatcher/Dispatcher.php
@@ -25,7 +25,7 @@ private $apiFactory;
 private $dbFactory;
 
 public function __construct($appNamespace, $request, $response,
-$apiFactory, $dbFactory, $templateFactory) {
+$apiFactory, $dbFactory) {
 	$this->appNamespace = $appNamespace;
 	$this->request = $request;
 	$this->response = $response;
