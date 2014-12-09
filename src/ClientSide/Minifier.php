31b6618da94cac0224450dfe7c2f805743859773
Greg Bowler <greg.bowler@g105b.com>
2014-12-09 15:25:01 +0000

    Add missing annotations

--- src/ClientSide/Minifier.php
+++ src/ClientSide/Minifier.php
@@ -11,6 +11,14 @@ use \Gt\Response\Response;
 
 class Minifier {
 
+/**
+ * Takes the provided input and minifies. This is a stub method for now until
+ * minification is required, and simply returns the unchanged input.
+ *
+ * @param string $input Input string
+ *
+ * @return string Minified output
+ */
 public function minify($input) {
 	$output = $input;
 
