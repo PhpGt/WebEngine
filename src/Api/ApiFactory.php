54e8d257e8633b7b9ea17475827ede8f83837e73
Greg Bowler <greg.bowler@g105b.com>
2014-12-18 18:18:50 +0000

    API development

--- src/Api/ApiFactory.php
+++ src/Api/ApiFactory.php
@@ -7,12 +7,61 @@
  */
 namespace Gt\Api;
 
+use \Gt\Core\Path;
+
 class ApiFactory {
 
 private $config;
+private $version;
+
+private $componentArray = [];
 
 public function __construct($config) {
 	$this->config = $config;
+	$this->version = $this->getVersionName($config->default_version);
+}
+
+public function __get($name) {
+	$name = strtolower($name);
+
+	if(array_key_exists($name, $this->componentArray)) {
+		return $this->componentArray[$name];
+	}
+
+	$component = new Component($name, $this->config);
+	$this->componentArray[$name] = $component;
+
+	return $this->componentArray[$name];
+}
+
+/**
+ * @param int $version The numerical version of the current API to use
+ *
+ * @return string The version name set according to provided version number
+ */
+public function setVersion($version) {
+	if(!is_int($version)) {
+		throw new \Gt\Core\InvalidArgumentException($version);
+	}
+
+	return $this->version = $this->getVersionName($version);
+}
+
+/**
+ * @param int $version The numerical version of the current API
+ *
+ * @return string The version name according to the provided number
+ */
+private function getVersionName($version) {
+	return $this->config->version_prefix
+		. $version
+		. $this->config->version_suffix;
+}
+
+private function getBaseDirectory() {
+	$path = Path::get(Path::API) . "/" . $this->version;
+	$path = Path::fixCase($path);
+	return $path;
 }
 
 }#
---
+++ src/Api/Component.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ *
+ * PHP.Gt (http://php.gt)
+ * @copyright Copyright Ⓒ 2014 Bright Flair Ltd. (http://brightflair.com)
+ * @license Apache Version 2.0, January 2004. http://www.apache.org/licenses
+ */
+namespace Gt\Api;
+
+class Component {
+
+private $name;
+private $config;
+private $parent;
+
+public function __construct($name, $config, $parent = null) {
+	$this->name = $name;
+	$this->config = $config;
+	$this->parent = $parent;
+}
+
+public function __get($name) {
+	return new Component($name, $this->config, $this);
+}
+
+public function __call($name, $args) {
+	$path = $this->getPath();
+	var_dump($name, $args);die();
+}
+
+private function getPath() {
+	$path = "";
+
+	$reference = $this;
+	while(!is_null($reference->parent)) {
+		$path = $reference->getName() . "/$path";
+
+		$reference = $reference->getParent();
+	}
+
+	var_dump($path);die("!!!!!!!!!!!!!");
+}
+
+public function getParent() {
+	return $this->parent;
+}
+
+public function getName() {
+	return $this->name;
+}
+
+}#
