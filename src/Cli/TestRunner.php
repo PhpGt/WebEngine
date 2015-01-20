d367d8dec1752c655e94eb4b15d059fbd87ff45b
Greg Bowler <greg.bowler@g105b.com>
2015-01-20 18:14:44 +0000

    Implement acceptance test runner

--- src/Cli/TestRunner.php
+++ src/Cli/TestRunner.php
@@ -5,6 +5,7 @@
  * @license Apache Version 2.0, January 2004. http://www.apache.org/licenses
  */
 namespace Gt\Cli;
+use \Gt\Core\Path;
 
 class TestRunner {
 
@@ -15,8 +16,26 @@ const TYPE_ACCEPTANCE = "type_acceptance";
 const ERRORLEVEL_UNIT = 1;
 const ERRORLEVEL_ACCEPTANCE = 2;
 
-public function __construct($applicationRoot, $type) {
+private $approot;
+private $type;
+
+private $countUnit = 0;
+private $countAcceptance = 0;
+
+private $descriptorSpec = [
+	0 => ["pipe", "r"],
+	1 => ["pipe", "w"],
+	2 => ["pipe", "w"],
+];
+
+public function __construct($approot, $type) {
 	$type = strtolower("TYPE_" . $type);
+
+	$this->approot = $approot;
+	$this->type = $type;
+
+	$unitResult = 0;
+	$acceptanceResult = 0;
 
 	switch ($type) {
 	case self::TYPE_ALL:
@@ -37,10 +56,30 @@ public function __construct($applicationRoot, $type) {
 		break;
 	}
 
+	if($this->countAcceptance > 0) {
+		echo "\nAcceptance tests completed.";
+	}
+	else {
+		echo "\nNo acceptance tests have been run.";
+	}
+	if($this->countUnit > 0) {
+		echo "\nUnit tests completed.";
+	}
+	else {
+		echo "\nNo unit tests have been run.";
+	}
+
+	echo "\n";
+
 	$errorLevel = $unitResult | $acceptanceResult;
-	$errorLevel = 3;
+
 	if($errorLevel == 0) {
-		echo "\n ✓ SUCCESS : All tests passing.";
+		if($this->countUnit + $this->countAcceptance === 0) {
+			echo "\n - NO TESTS : No tests have been detected.";
+		}
+		else {
+			echo "\n ✓ SUCCESS : All tests passing.";
+		}
 	}
 	else {
 		echo "\n ✗ FAILURE : ";
@@ -64,12 +103,63 @@ public function __construct($applicationRoot, $type) {
 	exit($errorLevel);
 }
 
+/**
+ *
+ */
 private function testUnit() {
 
 }
 
+/**
+ *
+ */
 private function testAcceptance() {
+	$result = 0;
+	$baseCwd = getcwd();
+	$gtroot = Path::get(Path::GTROOT);
+	$testPath = Path::fixCase($this->approot . "/test/Acceptance");
 
+	if(!is_dir($testPath)) {
+		return 0;
+	}
+
+	$testWebroot = "$testPath/www";
+	if(!is_dir($testWebroot)) {
+		mkdir($testWebroot, 0775, true);
+	}
+
+	$serverCommand = "$gtroot/bin/serve --approot=$testPath --port=8089";
+	$server = proc_open($serverCommand, $this->descriptorSpec, $pipes);
+
+	chdir($testPath);
+	$test = system("$gtroot/vendor/bin/behat", $exitCode);
+
+	// Inbuilt server spawns child processes that need killing.
+	$status = proc_get_status($server);
+	$pid = $status["pid"];
+	$pidArray = [];
+
+	while(!empty($pid)) {
+		$pidArray []= $pid;
+		$pid = exec("pgrep -P $pid");
+	}
+
+	proc_terminate($server);
+	proc_close($server);
+	foreach ($pidArray as $p) {
+		posix_kill($p, SIGKILL);
+	}
+
+	if($exitCode == 0) {
+		$this->countAcceptance++;
+	}
+	else {
+		$result = self::ERRORLEVEL_ACCEPTANCE;
+	}
+
+	// Reset the cwd.
+	chdir($baseCwd);
+	return $result;
 }
 
 }#
