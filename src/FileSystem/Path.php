95a7ac3c1a8ca927bb940d8a38a3d50ff9ad9ae8
Greg Bowler <greg.bowler@g105b.com>
2017-11-02 12:47:37 +0000

    Begin testing path getters

--- src/FileSystem/Path.php
+++ src/FileSystem/Path.php
@@ -2,18 +2,7 @@
 namespace Gt\FileSystem;
 
 class Path {
-	const ROOT			= "root";
-	const GTROOT			= "gtroot";
-	const SRC			= "src";
-	const WWW			= "www";
-	const PAGE			= "page";
-	const API			= "api";
-	const DATA			= "data";
-	const ASSET			= "asset";
-	const SCRIPT			= "script";
-	const STYLE			= "style";
-
-	public static function getApplicationRoot(string $documentRoot = null):string {
+	public static function getApplicationRootDirectory(string $documentRoot = null):string {
 		if(is_null($documentRoot)) {
 			$documentRoot = $_SERVER["DOCUMENT_ROOT"];
 		}
@@ -25,6 +14,56 @@ class Path {
 		}
 
 		$directoryWalker = new DirectoryWalker($path);
-		return $directoryWalker->findParentContaining("src");
+
+		return self::fixPathCase(
+			$directoryWalker->findParentContaining("src")
+		);
+	}
+
+	public static function getGtRootDirectory():string {
+		// TODO.
+	}
+
+	public static function getSrcDirectory():string {
+		// TODO.
+	}
+
+	public static function getWwwDirectory():string {
+		// TODO.
+	}
+
+	public static function getPageDirectory():string {
+		// TODO.
+	}
+
+	public static function getApiDirectory():string {
+		// TODO.
+	}
+
+	public static function getDataDirectory():string {
+		// TODO.
+	}
+
+	public static function getAssetDirectory():string {
+		// TODO.
+	}
+
+	public static function getScriptDirectory():string {
+		// TODO.
+	}
+
+	public static function getStyleDirectory():string {
+		// TODO.
+	}
+
+	public static function getChildOfSrcDirectory(string $name):string {
+		return self::fixPathCase(implode(DIRECTORY_SEPARATOR, [
+			self::getSrcDirectory(),
+			$name,
+		]));
+	}
+
+	public static function fixPathCase(string $path):string {
+		// TODO.
 	}
 }
---
+++ test/unit/FileSystem/PathTest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace unit\FileSystem;
+
+use Gt\FileSystem\Path;
+use PHPUnit\Framework\TestCase;
+
+class PathTest extends TestCase {
+
+}
