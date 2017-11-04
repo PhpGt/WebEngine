c694f207166e0c912a145ec72219a6e6f4a14fac
Greg Bowler <greg.bowler@g105b.com>
2017-11-04 17:54:08 +0000

    Add docs todo

--- src/Lifecycle.php
+++ src/Lifecycle.php
@@ -9,6 +9,7 @@ class Lifecycle {
 		$request = RequestFactory::create($_SERVER);
 		$dispatcher = DispatcherFactory::create($request);
 		// TODO: Input should be an object representing any user input (GET, POST, PUT, FILES, etc).
+		// TODO: Document why this kind of thing is an instance rather than static class - encapsulation
 		$dispatcher->go($request, $input);
 	}
 
