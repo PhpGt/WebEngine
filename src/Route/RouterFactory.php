83fd331d32dddef7259a5f807ff0f58d98b515cb
Greg Bowler <greg.bowler@g105b.com>
2017-11-05 22:32:29 +0000

    Stub Router class

--- src/Route/RouterFactory.php
+++ src/Route/RouterFactory.php
@@ -1,10 +1,11 @@
 <?php
-namespace Gt\WebEngine;
+namespace Gt\WebEngine\Route;
 
 use Psr\Http\Message\RequestInterface;
 
 class RouterFactory {
 	public static function create(RequestInterface $request):Router {
 		$router = new Router($request);
+		return $router;
 	}
 }
