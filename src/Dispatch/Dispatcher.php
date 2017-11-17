411f6234312e52c8b5eee543fc8d5df645a12fcd
Greg Bowler <greg.bowler@g105b.com>
2017-11-17 23:50:34 +0000

    Handle request _and_ response

--- src/Dispatch/Dispatcher.php
+++ src/Dispatch/Dispatcher.php
@@ -3,6 +3,7 @@ namespace Gt\WebEngine\Dispatch;
 
 use Gt\WebEngine\Route\Router;
 use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
 
 class Dispatcher {
 	/** @var Router */
@@ -11,9 +12,10 @@ class Dispatcher {
 	public function __construct(Router $router) {
 		$this->router = $router;
 	}
-	public function handle(RequestInterface $request) {
+	public function handle(RequestInterface $request, ResponseInterface $response) {
 		$path = $request->getUri()->getPath();
-		$baseViewLogicPath = $this->router->getBaseViewLogicPath();
-		$viewLogicSubPath = $this->router->getViewLogicSubPath($path);
+		$viewFile = $this->router->getViewFile($path);
+
+		var_dump($viewFile);die();
 	}
 }
