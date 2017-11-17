5b8e82b43acb49e9cda9f426da2a3b91d9aeb0ba
Greg Bowler <greg.bowler@g105b.com>
2017-11-17 22:56:53 +0000

    Router factory method

--- src/Route/RouterFactory.php
+++ src/Route/RouterFactory.php
@@ -2,10 +2,23 @@
 namespace Gt\WebEngine\Route;
 
 use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
 
 class RouterFactory {
-	public static function create(RequestInterface $request):Router {
-		$router = new Router($request);
+	protected static $routerClassLookup = [];
+
+	public static function registerRouterClassForResponse(
+		string $routerClassName,
+		string $responseClassName
+	):void {
+		static::$routerClassLookup[$responseClassName] = $routerClassName;
+	}
+
+	public static function create(
+		RequestInterface $request,
+		ResponseInterface $response
+	):Router {
+		$router = new static::$routerClassLookup[get_class($response)]();
 		return $router;
 	}
 }
