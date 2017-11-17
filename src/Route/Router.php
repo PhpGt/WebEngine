556f4ef97e9244226e58486c26b1dfbe43363745
Greg Bowler <greg.bowler@g105b.com>
2017-11-17 17:01:56 +0000

    Return default URI for the time being

--- src/Route/Router.php
+++ src/Route/Router.php
@@ -1,5 +1,12 @@
 <?php
 namespace Gt\WebEngine\Route;
 
+use Gt\Http\Uri;
+use Psr\Http\Message\UriInterface;
+
 class Router {
+	public function getCorrectedUri(UriInterface $requestedUri):UriInterface {
+		// TODO: Build up the corrected URI according to the path, etc.
+		return new Uri("/");
+	}
 }
