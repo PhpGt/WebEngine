feb6b82ad14760ea393a8d0990c01a940f2085f6
Greg Bowler <greg.bowler@g105b.com>
2017-12-30 19:19:28 +0000

    Stream the viewmodel back to the response

--- src/View/View.php
+++ src/View/View.php
@@ -1,12 +1,20 @@
 <?php
 namespace Gt\WebEngine\View;
 
+use Psr\Http\Message\StreamInterface;
+
 abstract class View {
+	protected $outputStream;
 	protected $viewModel;
 
-	public function __construct($viewModel) {
+	public function __construct(StreamInterface $outputStream, $viewModel) {
+		$this->outputStream = $outputStream;
 		$this->viewModel = $viewModel;
 	}
 
 	abstract public function getViewModel();
+
+	public function stream():void {
+		$this->outputStream->write((string)$this->viewModel);
+	}
 }
