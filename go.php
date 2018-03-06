b48d57249790c6f4496b7fe791490a2772b4f8d0
Greg Bowler <greg.bowler@g105b.com>
2018-03-06 21:23:39 +0000

    Windows compatibility

--- go.php
+++ go.php
@@ -21,7 +21,12 @@ if(is_file($_SERVER["DOCUMENT_ROOT"] . $uri)) {
  * rather than having to know where on disk the files exist.
  * @see https://getcomposer.org/doc/00-intro.md
  */
-require(__DIR__ . "/vendor/autoload.php");
+require(__DIR__
+	. DIRECTORY_SEPARATOR
+	. "vendor"
+	. DIRECTORY_SEPARATOR
+	. "autoload.php"
+);
 
 /**
  * That's all we need to start the request-response lifecycle. Buckle up and enjoy the ride!
